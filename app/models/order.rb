3a1b8aba54d8f6191ce6b3610ac12b418f9cc956
Ngoc Thoai Nguyen <ngocthoaia1@gmail.com>
2017-06-25 21:33:58 +0700

    add status i18n

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
 class Order < ApplicationRecord
   enum status: { "nopayment" => 0, "finished" => 1, "failed" => 2}
+  
+  class << self
+    def statuses_i18n
+      statuses.each_with_object({}) do |(k, _), obj|
+        obj[I18n.t("order.status.#{k}")] = k
+      end
+    end
+  end
+  
+  def status_i18n
+    I18n.t("order.status.#{status}")
+  end
 end
